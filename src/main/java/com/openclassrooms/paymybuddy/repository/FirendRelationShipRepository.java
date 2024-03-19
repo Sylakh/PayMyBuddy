daa8256c218cf4f9193f192e26c63ef4b4b9ed37
Sylakh <sylvainsds@gmail.com>
2024-03-19 19:23:50 +0100

    creation of repositories

--- src/main/java/com/openclassrooms/paymybuddy/repository/FirendRelationShipRepository.java
+++ src/main/java/com/openclassrooms/paymybuddy/repository/FirendRelationShipRepository.java
@@ -1,9 +1,11 @@
 package com.openclassrooms.paymybuddy.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.openclassrooms.paymybuddy.model.FriendRelationShip;
 
+@Repository
 public interface FirendRelationShipRepository extends CrudRepository<FriendRelationShip, Long> {
 
 }
